83aa386b5453deb7573f192fb9b08ff60b14aa81
Jude Baca-Miller <judeamiller@gmail.com>
2021-01-17 12:52:52 -0700

    cleanup types

--- src/components/Graph.vue
+++ src/components/Graph.vue
@@ -1,17 +1,46 @@
 <template>
   <div>
-    <Plotly :data="data" :layout="layout" :display-mode-bar="false"></Plotly>
+    <Plotly :data="cities" :layout="layout" :display-mode-bar="false"></Plotly>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
 import { Plotly } from 'vue-plotly';
+import censusData from '@/assets/data/converted-data.json';
+import { CensusDataType, LineDataType } from '@/types';
 
 @Component({
-  components: {Plotly}
+  components: { Plotly },
 })
 export default class Graph extends Vue {
+  censusData: CensusDataType = censusData;
+  cities: LineDataType[] = [];
+
+  mounted() {
+    for (const key of Object.keys(this.censusData)) {
+      //@ts-ignore
+      const data = this.censusData[key];
+      const lineData = {
+        x: [2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019],
+        y: [
+          data['2011'],
+          data['2012'],
+          data['2013'],
+          data['2014'],
+          data['2015'],
+          data['2016'],
+          data['2017'],
+          data['2018'],
+          data['2019'],
+        ],
+        name: data.City,
+        type: 'Scatter'
+      };
+      this.cities.push(lineData)
+
+    }
+  }
   data = [
     {
       x: [1, 2, 3, 4],
@@ -20,7 +49,7 @@ export default class Graph extends Vue {
     },
   ];
   layout = {
-    title: 'My graph',
+    title: 'Median Household Incomes by Most Populous U.S. Metropolitan Areas',
   };
 }
 </script>
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,18 +1,50 @@
 export interface CensusDataType {
-  [key: number]: {
-    City: string;
-    2011: number;
-    2012: number;
-    2013: number;
-    2014: number;
-    2015: number;
-    2016: number;
-    2017: number;
-    2018: number;
-    2019: number;
-    'Unnamed: 10': number; //TODO: cleanup py script to skip unused cols.
-    'Growth 2011-2019': number;
-    'Unnamed: 12': number;
-    'Growth 2015-2019': number;
-  };
+  0: CityType;
+  1: CityType;
+  2: CityType;
+  3: CityType;
+  4: CityType;
+  5: CityType;
+  6: CityType;
+  7: CityType;
+  8: CityType;
+  9: CityType;
+  10: CityType;
+  11: CityType;
+  12: CityType;
+  13: CityType;
+  14: CityType;
+  15: CityType;
+  17: CityType;
+  18: CityType;
+  19: CityType;
+  20: CityType;
+  21: CityType;
+  22: CityType;
+  23: CityType;
+  24: CityType;
+}
+
+export interface CityType {
+  City: string;
+  2011: number;
+  2012: number;
+  2013: number;
+  2014: number;
+  2015: number;
+  2016: number;
+  2017: number;
+  2018: number;
+  2019: number;
+  'Unnamed: 10': number; //TODO: cleanup py script to skip unused cols.
+  'Growth 2011-2019': number;
+  'Unnamed: 12': number;
+  'Growth 2015-2019': number;
+}
+
+export interface LineDataType {
+  x: number[];
+  y: number[];
+  name: string;
+  type: string;
 }
